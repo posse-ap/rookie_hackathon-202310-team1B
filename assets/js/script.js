96968abb6a583baf259563a4cb6a7e8356b57180
rumi.shikiya@gmail.com <rumi.shikiya@gamil.com>
2023-11-18 23:35:56 +0900

    メインビジュアル

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,24 @@ window.addEventListener("scroll", function(){
       header.classList.add("is-transparent");}
     }
   );
+
+  $(document).ready(function(){
+    $(document).ready(function () {
+        $('.slider').slick({
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            speed: 1000,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            prevArrow: '<div class="slick-prev"></div>',
+            nextArrow: '<div class="slick-next"></div>',
+            dots: true,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            pauseOnDotsHover: false,
+    });
+    });
+
