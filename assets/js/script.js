c7130ba0f8b39316fdf2a03637e10bb2254c5d3e
ReonGoda <goda.reon@keio.jp>
2023-11-18 20:29:32 +0900

    header中途完成

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/* ハンバーガーメニュー */
+
+// ヘッダー・ボタンの要素を取得
+const header = document.getElementById("js-header");
+const button = document.getElementById("js-headerButton");
+const navLinks = document.querySelectorAll('.header-navLink');
+
+// ボタンをクリックした時の処理
+button.addEventListener("click",()=>{
+    header.classList.toggle("is-open");
+})
+// メニューの各リンクをクリックしたときの処理
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    header.classList.remove('is-open');
+  });
+});
+
+/* ヘッダーの透過 */
+const mainVisual = document.getElementById("js-mainVisual");
