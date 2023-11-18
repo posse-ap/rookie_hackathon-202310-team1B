3c22bf4ea6fcb14f313da189ec93f2a59263a366
rumi.shikiya@gmail.com <rumi.shikiya@gamil.com>
2023-11-19 00:35:01 +0900

    メインビジュアル一個になったよ

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,22 +62,64 @@ if (mainVisual) {
 }
 
 
-// Slick sliderの初期化（このコードがDOMが準備された後に実行されることを確認してください）
-$(document).ready(function () {
-    $('.slider').slick({
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        speed: 1000,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: '<div class="slick-prev"></div>',
-        nextArrow: '<div class="slick-next"></div>',
-        dots: true,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        pauseOnDotsHover: false,
+$('.slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: true,//左右の矢印あり
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+/* ハンバーガーメニュー */
+
+
+// ヘッダー・ボタンの要素を取得
+const header = document.getElementById("js-header");
+const button = document.getElementById("js-headerButton");
+const navLinks = document.querySelectorAll('.header-navLink');
+
+// ボタンをクリックした時の処理
+button.addEventListener("click", () => {
+    header.classList.toggle("is-open");
+});
+
+// メニューの各リンクをクリックしたときの処理
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        header.classList.remove('is-open');
     });
 });
+
+/* ヘッダーの透過 */
+const mainVisual = document.getElementById("js-mainVisual");
+window.addEventListener("scroll", function () {
+    if (window.scrollY > mainVisual.clientHeight - header.clientHeight) {
+        header.classList.remove("is-transparent");
+    } else {
+        header.classList.add("is-transparent");
+    }
+});
+
+if (mainVisual) {
+    // mainVisualがnullでない場合の処理
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > mainVisual.clientHeight - header.clientHeight) {
+            header.classList.remove("is-transparent");
+        } else {
+            header.classList.add("is-transparent");
+        }
+    });
+} else {
+    console.error("mainVisualが見つかりませんでした。");
+}
+
+
